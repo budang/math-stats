3a0aaaed9c3e959735e6ce7909230cb96e491705
Brandon <budang@utexas.edu>
2016-08-22 11:29:46 -0500

    more refactoring

--- dist/discrete/binomial.js
+++ dist/discrete/binomial.js
@@ -7,17 +7,13 @@ exports.Binomial = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _bigJs = require('big-js');
+
+var _bigJs2 = _interopRequireDefault(_bigJs);
+
 var _choose2 = require('../internal/_choose');
 
 var _choose3 = _interopRequireDefault(_choose2);
-
-var _multiply2 = require('../internal/_multiply');
-
-var _multiply3 = _interopRequireDefault(_multiply2);
-
-var _pow2 = require('../internal/_pow');
-
-var _pow3 = _interopRequireDefault(_pow2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -34,9 +30,9 @@ var Binomial = function () {
   function Binomial(p, n) {
     _classCallCheck(this, Binomial);
 
-    this.p = p;
-    this.q = 1 - p;
-    this.n = n;
+    this.p = new _bigJs2.default(p);
+    this.q = new _bigJs2.default(1 - p);
+    this.n = new _bigJs2.default(n);
 
     this.mean = this._mean();
     this.variance = this._variance();
@@ -53,7 +49,7 @@ var Binomial = function () {
     value: function probability(y) {
       if (y < 0 || y > this.n) throw new Error("Invalid value for y");
 
-      return (0, _multiply3.default)((0, _choose3.default)(this.n, y), (0, _pow3.default)(this.p, y), (0, _pow3.default)(this.q, this.n - y));
+      return parseFloat(new _bigJs2.default((0, _choose3.default)(this.n, y)).times(this.p.pow(y)).times(this.q.pow(parseFloat(this.n.minus(y)))));
     }
 
     /**
@@ -63,7 +59,7 @@ var Binomial = function () {
   }, {
     key: '_mean',
     value: function _mean() {
-      return (0, _multiply3.default)(this.n, this.p);
+      return parseFloat(this.n.times(this.p));
     }
 
     /**
@@ -73,7 +69,7 @@ var Binomial = function () {
   }, {
     key: '_variance',
     value: function _variance() {
-      return (0, _multiply3.default)(this.n, this.p, this.q);
+      return parseFloat(this.n.times(this.p).times(this.q));
     }
   }]);
 
