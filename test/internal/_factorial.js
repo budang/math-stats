801f75389f8e780d70aac547275babb8c09fb282
Brandon <budang@utexas.edu>
2016-08-28 11:22:00 -0500

    adding tests

--- test/internal/_factorial.js
+++ test/internal/_factorial.js
@@ -8,10 +8,20 @@ const
  * _factorial function
  */
 describe('_factorial()', () => {
-  const n = 10;
+  const n = [10, 0, -5];
 
-  it(`calculates factorial of n = ${n}`, () => {
-    const factorial = _factorial(n);
+  it(`calculates factorial of n = ${n[0]}`, () => {
+    const factorial = _factorial(n[0]);
     assert.equal(factorial, 3628800);
+  });
+
+  it(`calculates factorial of n = ${n[1]}`, () => {
+    const factorial = _factorial(n[1]);
+    assert.equal(factorial, 1);
+  });
+
+  it(`calculates factorial of n = ${n[2]}`, () => {
+    const factorial = _factorial(n[2]);
+    assert.equal(factorial, Infinity);
   });
 });
