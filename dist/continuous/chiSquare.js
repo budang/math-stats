f1385c7980d48c1f9214e6645719cfe938b4d946
Brandon <budang@utexas.edu>
2016-08-31 22:01:01 -0500

    refactoring chi square rv

--- dist/continuous/chiSquare.js
+++ dist/continuous/chiSquare.js
@@ -7,13 +7,13 @@ exports.ChiSquare = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _big = require('big.js');
+
+var _big2 = _interopRequireDefault(_big);
+
 var _gamma2 = require('../internal/_gamma');
 
 var _gamma3 = _interopRequireDefault(_gamma2);
-
-var _multiply2 = require('../internal/_multiply');
-
-var _multiply3 = _interopRequireDefault(_multiply2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -23,7 +23,7 @@ var ChiSquare = function () {
   function ChiSquare(v) {
     _classCallCheck(this, ChiSquare);
 
-    this.v = v;
+    this.v = new _big2.default(v);
 
     this.mean = this._mean();
     this.variance = this._variance();
@@ -34,17 +34,17 @@ var ChiSquare = function () {
     value: function probability(y) {
       if (Math.pow(y, 2) <= 0) throw new Error("Invalid value for y");
 
-      return (0, _multiply3.default)(Math.pow(y, this.v / 2), Math.E(-y / 2)) / (0, _multiply3.default)((Math.pow(2, this.v / 2), (0, _gamma3.default)(this.v / 2)));
+      return parseFloat(new _big2.default(y).pow(this.v.div(2)).times(Math.E(-y / 2)).div(new _big2.default(2).pow(this.v.div(2)).times((0, _gamma3.default)(this.v.div(2)))));
     }
   }, {
     key: '_mean',
     value: function _mean() {
-      return this.v;
+      return parseFloat(this.v);
     }
   }, {
     key: '_variance',
     value: function _variance() {
-      return 2 * this.v;
+      return parseFloat(this.v.times(2));
     }
   }]);
 
